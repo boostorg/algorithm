d4734356e92d31f4c1a3b235e4370f39a87a3b60
Marshall Clow <mclow.lists@gmail.com>
2014-12-03 15:15:15 -0800

    Added more general power functionality as requested by Sean Parent. Also added enable_if to make sure the exponent is an integral type.

--- test/power_test.cpp
+++ test/power_test.cpp
@@ -15,22 +15,21 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-#include <string>
-#include <vector>
-#include <list>
-
 namespace ba = boost::algorithm;
 
 BOOST_AUTO_TEST_CASE( test_main )
 {
-	BOOST_CHECK ( ba::power(0, 0) == 1);
-	BOOST_CHECK ( ba::power(5, 0) == 1);
-	BOOST_CHECK ( ba::power(1, 1) == 1);
-	BOOST_CHECK ( ba::power(1, 4) == 1);
-	BOOST_CHECK ( ba::power(3, 2) == 9);
-	BOOST_CHECK ( ba::power(2, 3) == 8);
-	BOOST_CHECK ( ba::power(3, 3) == 27);
-	BOOST_CHECK ( ba::power(2, 30) == 0x40000000);
-	BOOST_CHECK ( ba::power(5L, 10) == 3125*3125);
-	BOOST_CHECK ( ba::power(18, 3) == 18*18*18);
+    BOOST_CHECK ( ba::power(0, 0) == 1);
+    BOOST_CHECK ( ba::power(5, 0) == 1);
+    BOOST_CHECK ( ba::power(1, 1) == 1);
+    BOOST_CHECK ( ba::power(1, 4) == 1);
+    BOOST_CHECK ( ba::power(3, 2) == 9);
+    BOOST_CHECK ( ba::power(2, 3) == 8);
+    BOOST_CHECK ( ba::power(3, 3) == 27);
+    BOOST_CHECK ( ba::power(2, 30) == 0x40000000);
+    BOOST_CHECK ( ba::power(5L, 10) == 3125*3125);
+    BOOST_CHECK ( ba::power(18, 3) == 18*18*18);
+    
+    BOOST_CHECK ( ba::power(3,2) == ba::power(3,2, std::multiplies<int>()));
+    BOOST_CHECK ( ba::power(3,2, std::plus<int>()) == 6);
 }
