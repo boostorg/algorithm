14392f87721b8b1c4ba8565d07faa384effb23f8
Alexander Zaitsev <zamazan4ik@gmail.com>
2016-08-15 03:32:51 +0300

    Added in doc 'insert' method

--- include/boost/algorithm/searching/aho_corasick.hpp
+++ include/boost/algorithm/searching/aho_corasick.hpp
@@ -22,176 +22,176 @@
 
 namespace boost { namespace algorithm {
 
-    template <typename T, template<typename ...> typename Container, typename ...Args>
-    class AhoCorasick
+template <typename T, template<typename ...> typename Container, typename ...Args>
+class AhoCorasick
+{
+private:
+    class MapBorNode
     {
-    private:
-        class MapBorNode
-        {
-        public:
-            Container<T, std::shared_ptr<MapBorNode>, Args...> links;
-            std::shared_ptr<MapBorNode> fail, term;
-            std::vector<size_t> pat;
-
-            MapBorNode(std::shared_ptr<MapBorNode> fail_node = nullptr)
-                    : fail(fail_node), term(nullptr)
-            { }
-
-            std::shared_ptr<MapBorNode> getLink(const T& c) const
-            {
-                const auto iter = links.find(c);
-                return iter != links.cend() ? iter->second : nullptr;
-            }
-
-            bool isTerminal() const
-            {
-                return !pat.empty();
-            }
-        };
     public:
-        using value_type = T;
-        using node_type = MapBorNode;
-    private:
-        std::shared_ptr<node_type> root, current_state;
-        size_t countStrings = 0;
-    public:
-        AhoCorasick() : root(std::make_shared<node_type>()) {}
+        Container<T, std::shared_ptr<MapBorNode>, Args...> links;
+        std::shared_ptr<MapBorNode> fail, term;
+        std::vector<size_t> pat;
 
-        template<typename ForwardIterator>
-        AhoCorasick(ForwardIterator patBegin, ForwardIterator patEnd) : root(std::make_shared<node_type>())
+        MapBorNode(std::shared_ptr<MapBorNode> fail_node = nullptr)
+                : fail(fail_node), term(nullptr)
+        { }
+
+        std::shared_ptr<MapBorNode> getLink(const T& c) const
         {
-            while(patBegin != patEnd)
-            {
-                insert(*patBegin);
-                ++patBegin;
-            }
+            const auto iter = links.find(c);
+            return iter != links.cend() ? iter->second : nullptr;
         }
 
-        /// \fn insert(const Range& range)
-        /// \brief Insert pattern in trie
-        ///
-        /// \param range The pattern range
-        ///
-        template<typename Range>
-        void insert(const Range& range)
+        bool isTerminal() const
         {
-            insert(boost::begin(range), boost::end(range));
-        }
-
-        /// \fn insert(ForwardIterator begin, ForwardIterator end)
-        /// \brief Insert pattern in trie
-        ///
-        /// \param begin The start of the pattern
-        /// \param end   One past the end of the pattern
-        ///
-        template<typename ForwardIterator>
-        void insert(ForwardIterator begin, ForwardIterator end)
-        {
-            size_t patLen = 0;
-            std::shared_ptr<node_type> current_node = root;
-            for(auto it = begin; it != end; ++it)
-            {
-                ++patLen;
-                std::shared_ptr<node_type> child_node = current_node->getLink(*it);
-                if (!child_node)
-                {
-                    child_node = std::make_shared<node_type>(root);
-                    current_node->links[*it] = child_node;
-                }
-                current_node = child_node;
-            }
-            current_node->pat.push_back(patLen);
-        }
-
-        /// \fn operator (RAIterator begin, RAIterator end, Out& cont)
-        /// \brief Searches patterns in the corpus
-        ///
-        /// \param begin The start of the data to search (Random Access Iterator)
-        /// \param end   One past the end of the data to search (Random Access Iterator)
-        /// \param cont  Output container of pairs of iterators to corpus sequence
-        ///
-        template <typename RAIterator, typename Out>
-        void operator()(RAIterator begin, RAIterator end, Out& cont)
-        {
-            init();
-            current_state = root;
-            for(auto it = begin; it != end; ++it)
-            {
-                step(*it);
-                getTermsForCurrentState(it, cont);
-            }
-        }
-    private:
-        void init()
-        {
-            std::queue<std::shared_ptr<node_type>> q;
-            q.push(root);
-            while (!q.empty())
-            {
-                std::shared_ptr<node_type> current_node = q.front();
-                q.pop();
-                for (auto iter = current_node->links.cbegin();
-                     iter != current_node->links.cend(); ++iter)
-                {
-                    const value_type& symbol = iter->first;
-                    std::shared_ptr<node_type> child = iter->second;
-
-                    // Defining .fail for the childnode
-                    std::shared_ptr<node_type> temp_node = current_node->fail;
-                    while (temp_node)
-                    {
-                        std::shared_ptr<node_type> fail_candidate = temp_node->getLink(symbol);
-                        if (fail_candidate)
-                        {
-                            child->fail = fail_candidate;
-                            break;
-                        }
-                        temp_node = temp_node->fail;
-                    }
-
-                    // Defining .term for the childnode using .term of current node
-                    child->term = (child->fail == nullptr || child->fail->isTerminal()) ? child->fail : child->fail->term;
-                    q.push(child);
-                }
-            }
-        }
-
-        void step(const value_type& c)
-        {
-            while (current_state)
-            {
-                std::shared_ptr<node_type> candidate = current_state->getLink(c);
-                if (candidate)
-                {
-                    current_state = candidate;
-                    return;
-                }
-                current_state = current_state->fail;
-            }
-            current_state = root;
-        }
-
-        template <typename RAIterator, typename Out>
-        void getTermsForCurrentState(RAIterator pos, Out& cont)
-        {
-            if (current_state->isTerminal())
-            {
-                for (const auto value : current_state->pat)
-                {
-                    cont.push_back({1 + pos - value, pos + 1});
-                }
-            }
-            std::shared_ptr<node_type> temp_node = current_state->term;
-            while (temp_node)
-            {
-                for (const auto value : temp_node->pat)
-                {
-                    cont.push_back({1 + pos - value, pos + 1});
-                }
-                temp_node = temp_node->term;
-            }
+            return !pat.empty();
         }
     };
+public:
+    using value_type = T;
+    using node_type = MapBorNode;
+private:
+    std::shared_ptr<node_type> root, current_state;
+    size_t countStrings = 0;
+public:
+    AhoCorasick() : root(std::make_shared<node_type>()) {}
+
+    template<typename ForwardIterator>
+    AhoCorasick(ForwardIterator patBegin, ForwardIterator patEnd) : root(std::make_shared<node_type>())
+    {
+        while(patBegin != patEnd)
+        {
+            insert(*patBegin);
+            ++patBegin;
+        }
+    }
+
+    /// \fn insert(const Range& range)
+    /// \brief Insert pattern in trie
+    ///
+    /// \param range The pattern range
+    ///
+    template<typename Range>
+    void insert(const Range& range)
+    {
+        insert(boost::begin(range), boost::end(range));
+    }
+
+    /// \fn insert(ForwardIterator begin, ForwardIterator end)
+    /// \brief Insert pattern in trie
+    ///
+    /// \param begin The start of the pattern
+    /// \param end   One past the end of the pattern
+    ///
+    template<typename ForwardIterator>
+    void insert(ForwardIterator begin, ForwardIterator end)
+    {
+        size_t patLen = 0;
+        std::shared_ptr<node_type> current_node = root;
+        for(auto it = begin; it != end; ++it)
+        {
+            ++patLen;
+            std::shared_ptr<node_type> child_node = current_node->getLink(*it);
+            if (!child_node)
+            {
+                child_node = std::make_shared<node_type>(root);
+                current_node->links[*it] = child_node;
+            }
+            current_node = child_node;
+        }
+        current_node->pat.push_back(patLen);
+    }
+
+    /// \fn operator ( RAIterator begin, RAIterator end, Out& cont)
+    /// \brief Searches patterns in the corpus
+    ///
+    /// \param begin The start of the data to search (Random Access Iterator)
+    /// \param end   One past the end of the data to search (Random Access Iterator)
+    /// \param cont  Output container of pairs of iterators to corpus sequence
+    ///
+    template <typename RAIterator, typename Out>
+    void operator()(RAIterator begin, RAIterator end, Out& cont)
+    {
+        init();
+        current_state = root;
+        for(auto it = begin; it != end; ++it)
+        {
+            step(*it);
+            getTermsForCurrentState(it, cont);
+        }
+    }
+private:
+    void init()
+    {
+        std::queue<std::shared_ptr<node_type>> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            std::shared_ptr<node_type> current_node = q.front();
+            q.pop();
+            for (auto iter = current_node->links.cbegin();
+                 iter != current_node->links.cend(); ++iter)
+            {
+                const value_type& symbol = iter->first;
+                std::shared_ptr<node_type> child = iter->second;
+
+                // Defining .fail for the childnode
+                std::shared_ptr<node_type> temp_node = current_node->fail;
+                while (temp_node)
+                {
+                    std::shared_ptr<node_type> fail_candidate = temp_node->getLink(symbol);
+                    if (fail_candidate)
+                    {
+                        child->fail = fail_candidate;
+                        break;
+                    }
+                    temp_node = temp_node->fail;
+                }
+
+                // Defining .term for the childnode using .term of current node
+                child->term = (child->fail == nullptr || child->fail->isTerminal()) ? child->fail : child->fail->term;
+                q.push(child);
+            }
+        }
+    }
+
+    void step(const value_type& c)
+    {
+        while (current_state)
+        {
+            std::shared_ptr<node_type> candidate = current_state->getLink(c);
+            if (candidate)
+            {
+                current_state = candidate;
+                return;
+            }
+            current_state = current_state->fail;
+        }
+        current_state = root;
+    }
+
+    template <typename RAIterator, typename Out>
+    void getTermsForCurrentState(RAIterator pos, Out& cont)
+    {
+        if (current_state->isTerminal())
+        {
+            for (const auto value : current_state->pat)
+            {
+                cont.push_back({1 + pos - value, pos + 1});
+            }
+        }
+        std::shared_ptr<node_type> temp_node = current_state->term;
+        while (temp_node)
+        {
+            for (const auto value : temp_node->pat)
+            {
+                cont.push_back({1 + pos - value, pos + 1});
+            }
+            temp_node = temp_node->term;
+        }
+    }
+};
 
 //Object interface
 template <typename T, typename Pred = std::less<T>>
