7230137d597426bca3bcd6f0b970e5d9ab39653b
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2016-09-07 22:41:09 +1000

    Simplify test to just find all matches of pattern in corpus.
    Distinct function to read the corpus because it is a multi-line file.
    The pattern files are multi-line too, but we're treating them as one per
    line.

--- test/search_test5.cpp
+++ test/search_test5.cpp
@@ -27,55 +27,43 @@
 typedef std::vector<char> vec;
 #define NUM_TRIES   100
 
-#define runOne(call, refDiff)   { \
-    std::clock_t bTime, eTime;                              \
-    bTime = std::clock ();                                  \
-    for ( i = 0; i < NUM_TRIES; ++i ) {                     \
-        res = boost::algorithm::call                        \
-            ( haystack.begin (), haystack.end (),           \
-                        needle.begin (), needle.end ());    \
-        if ( res != exp ) {                                 \
-            std::cout << "On run # " << i << " expected "   \
-                << exp.first - haystack.begin () << " got "       \
-                << res.first - haystack.begin () << std::endl;    \
-            throw std::runtime_error                        \
-                ( "Unexpected result from " #call );        \
-            }                                               \
-        }                                                   \
-    eTime = std::clock ();                                  \
-    printRes ( #call, eTime - bTime, refDiff ); }
-
 #define runHALObject(obj, trait, refDiff) { \
     std::clock_t bTime, eTime;                              \
     bTime = std::clock ();                                  \
-    boost::algorithm::obj<vec::const_iterator, vec::const_iterator, trait >             \
+    boost::algorithm::obj<vec::const_iterator, vec::const_iterator, trait >    \
                 s_o ( needle.begin (), needle.end ());      \
     for ( i = 0; i < NUM_TRIES; ++i ) {                     \
-        res = s_o ( haystack.begin (), haystack.end ());    \
-        if ( res != exp ) {                                 \
+        unsigned matches = 0;                               \
+        res = std::make_pair(haystack.begin(), needle.begin()); \
+        while ((res = s_o ( ++res.first, haystack.end ())) != exp)    \
+            matches++;                                      \
+        if ( matches != matches_exp ) {                     \
             std::cout << "On run # " << i << " expected "   \
-            << exp.first - haystack.begin () << " got "           \
-            << res.first - haystack.begin () << std::endl;        \
+            << matches_exp << " got "       \
+            << matches << std::endl;    \
             throw std::runtime_error                        \
-            ( "Unexpected result from " #obj " object" );   \
-            }                                               \
-        }                                                   \
+            ( "Unexpected result from " #obj );        \
+        }                                               \
+    }                                                   \
     eTime = std::clock ();                                  \
     printRes ( #obj " object", eTime - bTime, refDiff ); }
     
-    #define runObject(obj, refDiff) { \
+#define runObject(obj, refDiff) { \
     std::clock_t bTime, eTime;                              \
     bTime = std::clock ();                                  \
     boost::algorithm::obj <vec::const_iterator>             \
     s_o ( needle.begin (), needle.end ());      \
     for ( i = 0; i < NUM_TRIES; ++i ) {                     \
-        res = s_o ( haystack.begin (), haystack.end ());    \
-        if ( res != exp ) {                                 \
+        unsigned matches = 0;                               \
+        res = std::make_pair(haystack.begin(), needle.begin()); \
+        while ((res = s_o ( ++res.first, haystack.end ())) != exp)    \
+            matches++;                                      \
+        if ( matches != matches_exp ) {                                 \
             std::cout << "On run # " << i << " expected "   \
-            << exp.first - haystack.begin () << " got "           \
-            << res.first - haystack.begin () << std::endl;        \
+            << matches_exp << " got "       \
+            << matches << std::endl;    \
             throw std::runtime_error                        \
-            ( "Unexpected result from " #obj " object" );   \
+            ( "Unexpected result from " #obj );        \
         }                                               \
     }                                                   \
     eTime = std::clock ();                                  \
@@ -97,8 +85,23 @@ namespace {
         std::getline(in, temp);
         std::copy(temp.begin(), temp.end(), back_inserter(retVal));
         return retVal;
-        }
-    
+    }
+
+    vec ReadCorpus ( const char *name ) {
+        std::ifstream in ( name, std::ios_base::binary | std::ios_base::in );
+        vec retVal;
+        std::string temp;
+        /*
+            *        std::istream_iterator<char, char> begin(in);
+            *        std::istream_iterator<char, char> end;
+            *        
+            *        std::copy ( begin, end, std::back_inserter ( retVal ));
+            */
+        while (std::getline(in, temp))
+            std::copy(temp.begin(), temp.end(), back_inserter(retVal));
+        return retVal;
+    }
+        
     void printRes ( const char *prompt, unsigned long diff, unsigned long stdDiff ) {
         std::cout 
             << std::setw(34) << prompt << " "
@@ -110,52 +113,40 @@ namespace {
         std::cout << std::endl;
         }
     
-    void check_one ( const vec &haystack, const vec &needle, int expected ) {
+    void check_one ( const vec &haystack, const vec &needle, int ) {
         std::size_t i;
         std::clock_t sTime;
         unsigned long stdDiff;
         
         std::pair<vec::const_iterator, vec::const_iterator> res;
         std::pair<vec::const_iterator, vec::const_iterator> exp;        // the expected result
-        vec::const_iterator exp_start;
-        
-        if ( expected >= 0 )
-            exp_start = haystack.begin () + expected;
-        else if ( expected == -1 )
-            exp_start = haystack.end ();      // we didn't find it!
-        else if ( expected == -2 )
-            exp_start = std::search ( haystack.begin (), haystack.end (), needle.begin (), needle.end ());
-        else    
-            throw std::logic_error ( "Expected must be -2, -1, or >= 0" );
-
-		if ( expected == -1 )
-			exp = std::make_pair(haystack.end(), haystack.end());
-		else
-			exp = std::make_pair(exp_start, exp_start + needle.size());
-			
+        exp = std::make_pair(haystack.end(), haystack.end());
         std::cout << "Pattern is " << needle.size ()   << " entries long" << std::endl;
         std::cout << "Corpus  is " << haystack.size () << " entries long" << std::endl;
 
     //  First, the std library search
+        unsigned matches_exp;
         sTime = std::clock ();
         for ( i = 0; i < NUM_TRIES; ++i ) {
-            vec::const_iterator s_res = std::search ( haystack.begin (), haystack.end (), needle.begin (), needle.end ());
-            if ( s_res != exp.first ) {
-                std::cout << "On run # " << i << " expected " << exp.first - haystack.begin () << " got " << s_res - haystack.begin () << std::endl;
-                throw std::runtime_error ( "Unexpected result from std::search" );
-                }
+            matches_exp = 0;
+            vec::const_iterator foo = std::search ( haystack.begin (), haystack.end (), needle.begin (), needle.end ());
+            while (foo != haystack.end())
+            {
+                matches_exp++;
+                foo = std::search ( ++foo, haystack.end (), needle.begin (), needle.end ());
             }
+        }
         stdDiff = std::clock () - sTime;
         printRes ( "std::search", stdDiff, stdDiff );
 
         typedef boost::algorithm::search_trait<typename std::iterator_traits<vec::const_iterator>::value_type> default_mn_search_traits;
-        runOne    ( boyer_moore_search,          stdDiff );
+        // runOne    ( boyer_moore_search,          stdDiff );
         runObject ( boyer_moore,                 stdDiff );
-        runOne    ( boyer_moore_horspool_search, stdDiff );
+        // runOne    ( boyer_moore_horspool_search, stdDiff );
         runObject ( boyer_moore_horspool,        stdDiff );
-        runOne    ( knuth_morris_pratt_search,   stdDiff );
+        // runOne    ( knuth_morris_pratt_search,   stdDiff );
         runObject ( knuth_morris_pratt,          stdDiff );
-        runOne    ( musser_nishanov_HAL_search,   stdDiff );
+        // runOne    ( musser_nishanov_HAL_search,   stdDiff );
         runHALObject ( musser_nishanov_HAL, default_mn_search_traits, stdDiff );
         runHALObject ( musser_nishanov_HAL, boost::algorithm::search_trait_dna2, stdDiff );
         runHALObject ( musser_nishanov_HAL, boost::algorithm::search_trait_dna3, stdDiff );
@@ -167,7 +158,7 @@ BOOST_AUTO_TEST_CASE( test_main )
 {
     using namespace boost::filesystem;
     
-    vec c1  = ReadFromFile ( "search_test_data/DNA/0001.corpus" );    
+    vec c1  = ReadCorpus( "search_test_data/DNA/0001.corpus" );    
     std::cout << std::ios::fixed << std::setprecision(4);
     
     path data = path("search_test_data/DNA");
