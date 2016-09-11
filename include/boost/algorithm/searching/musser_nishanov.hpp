8ed94dea00587b1966383c6500e762ec7a38aa96
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2016-09-11 14:34:37 +1000

    Add missing type trait include.

--- include/boost/algorithm/searching/musser_nishanov.hpp
+++ include/boost/algorithm/searching/musser_nishanov.hpp
@@ -20,6 +20,7 @@
 #include <boost/mpl/or.hpp>
 #include <boost/next_prior.hpp>
 #include <boost/type_traits/is_base_of.hpp>
+#include <boost/type_traits/is_same.hpp>
 #include <boost/utility/enable_if.hpp>
 
 #include <iterator>
