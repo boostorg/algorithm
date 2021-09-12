9031f17bb818a1cb80c6185be1856466dd08bda9
Ivan Matek <libbooze@gmail.com>
2021-09-13 00:22:03 +0200

    update CR

--- include/boost/algorithm/is_clamped.hpp
+++ include/boost/algorithm/is_clamped.hpp
@@ -1,5 +1,5 @@
 /* 
-   Copyright (c) Marshall Clow 2008-2012.
+   Copyright (c) Ivan Matek, Marshall Clow 2021.
 
    Distributed under the Boost Software License, Version 1.0. (See accompanying
    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- test/is_clamped_test.cpp
+++ test/is_clamped_test.cpp
@@ -1,4 +1,4 @@
-﻿//  (C) Copyright TODO
+﻿//  (C) Copyright Ivan Matek, Marshall Clow 2021.
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
