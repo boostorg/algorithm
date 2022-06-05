2c5d484358604a7ceaef1bb1387b5c2a570c0dcf
Jonathan Gopel <jgopel@gmail.com>
2022-06-05 23:53:58 +0000

    Add dual-sequence implementation of transform_exclusive_scan()
    Problem:
    - `transform()` has an overload that takes two input sequences - in
      other languages this is called `zip()`.
    - `transform_reduce()` has an overload that takes two input sequences.
    - `transform_inclusive_scan()` has an overload that takes two input
      sequences.
    - `transform_exclusive_scan()` does not have this overload, despite its
      obvious utility from the other cases.
    
    Solution:
    - Implement an overload of `transform_exclusive_scan()` that takes two
      input ranges.

--- include/boost/algorithm/cxx17/transform_exclusive_scan.hpp
+++ include/boost/algorithm/cxx17/transform_exclusive_scan.hpp
@@ -58,6 +58,43 @@ OutputIterator transform_exclusive_scan(InputIterator first, InputIterator last,
     return result;
 }
 
+/// \fn transform_exclusive_scan ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, OutputIterator result, ScanOperation scan_op, TransformOperation trans_op, T init )
+/// \brief Transforms elements from the input range with uOp and then combines
+/// those transformed elements with bOp such that the n-1th element and the nth
+/// element are combined. Exclusivity means that the nth element is not
+/// included in the nth combination.
+/// \return The updated output iterator
+///
+/// \param first1   The start of the first input sequence
+/// \param last1    The end of the first input sequence
+/// \param first2   The start of the second input sequence
+/// \param result   The output iterator to write the results into
+/// \param scan_op  The operation for combining transformed input elements
+/// \param trans_op The operation for transforming pairs of input elements
+/// \param init     The initial value
+template<class InputIterator1, class InputIterator2, class OutputIterator,
+         class ScanOperation, class TransformOperation, class T>
+BOOST_CXX14_CONSTEXPR
+OutputIterator transform_exclusive_scan(InputIterator1 first1, InputIterator1 last1,
+                                        InputIterator2 first2,
+                                        OutputIterator result, T init,
+                                        ScanOperation scan_op, TransformOperation trans_op)
+{
+    if (first1 != last1)
+    {
+        T saved = init;
+        do
+        {
+            init = scan_op(init, trans_op(*first1, *first2));
+            *result = saved;
+            saved = init;
+            ++first2;
+            ++result;
+        } while (++first1 != last1);
+    }
+    return result;
+}
+
 }} // namespace boost and algorithm
 
 #endif // BOOST_ALGORITHM_TRANSFORM_EXCLUSIVE_SCAN_HPP
