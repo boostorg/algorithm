1d9706feb796e955aabf115051ef91676a417fb6
glywk <glywk.contact@gmail.com>
2023-01-21 00:42:52 +0100

    Add is_blank classifier

--- string/test/predicate_test.cpp
+++ string/test/predicate_test.cpp
@@ -138,7 +138,11 @@ void classification_test()
     TEST_CLASS( is_any_of( string("abc") ), "aaabbcc", "aaxb" );
     TEST_CLASS( is_any_of( "abc" ), "aaabbcc", "aaxb" );
     TEST_CLASS( is_from_range( 'a', 'c' ), "aaabbcc", "aaxb" );
-
+    
+#ifndef BOOST_NO_CXX11
+    TEST_CLASS( is_blank(), " \t", "\t \n\r" );
+#endif
+    
     TEST_CLASS( !is_classified(std::ctype_base::space), "...", "..\n\r\t " );
     TEST_CLASS( ( !is_any_of("abc") && is_from_range('a','e') ) || is_space(), "d e", "abcde" );
 
