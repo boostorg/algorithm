d73c8e1f2fd53ad045078753ec7c28e84f56e9ef
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2024-08-31 21:35:12 +1000

    Update copyright years

--- include/boost/algorithm/searching/musser_nishanov.hpp
+++ include/boost/algorithm/searching/musser_nishanov.hpp
@@ -1,6 +1,6 @@
 /* 
    Copyright (c) David R. Musser & Gor V. Nishanov 1997.
-   Copyright (c) Jeremy W. Murphy 2016.
+   Copyright (c) Jeremy W. Murphy 2024.
 
    Distributed under the Boost Software License, Version 1.0. (See accompanying
    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
